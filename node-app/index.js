39ad00576b25f8f96a3dd2c2db2923750f6654f4
mdtanvirteam <mdtanvir.ahmed@teamgrid.io>
2024-09-25 13:18:44 +0530

    routes added

--- node-app/index.js
+++ node-app/index.js
@@ -8,6 +8,11 @@ const path = require("path");
  
 const jobRoute = require("./routes/job-route");
 const userRoute = require("./routes/user-route");
+const pageRoute = require("./routes/pageRoutes");
+const pagegroupRoute = require("./routes/pagegroupRoutes");
+const permissionRoute = require("./routes/permissionRoutes");
+const roleRoute = require("./routes/roleRoutes");
+const rolepermissionRoute = require("./routes/rolepermissionRoutes");
  
  
 const PORT = process.env.PORT || 8080;
@@ -20,6 +25,11 @@ app.use(express.static("my-upload"));
 
 app.use("/job",jobRoute);
 app.use("/user",userRoute);
+app.use("/page",pageRoute);
+app.use("/pagegroup",pagegroupRoute);
+app.use("/permission",permissionRoute);
+app.use("/rolepermission",rolepermissionRoute);
+app.use("/role",roleRoute);
  
 app.listen(PORT, () => {
   console.log(`server has started at port ${PORT}`);
