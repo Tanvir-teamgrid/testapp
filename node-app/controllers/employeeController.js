c371920664f84dcfdcae2eb43de36f8a7d933849
mdtanvir200 <mdtanvir.ahmed@teamgrid.io>
2024-10-08 13:57:17 +0530

    updateEmployee function modified where the error was comming due to the reassingment of password due to const hashpassword

--- node-app/controllers/employeeController.js
+++ node-app/controllers/employeeController.js
@@ -160,6 +160,63 @@ class employeeController {
         .json({ message: "Error deleting employee", error: error.message });
     }
   };
+  static updateEmployee = async (req,res) => {
+    try {
+      const userId = req.params.id;
+      const {username,email,phone,roleId,firstName,lastName,dob,contactNumber,password}=req.body;
+      const user = await User.findById(userId)
+      if(!user)
+      {
+        return res.status(404).json({message:"employee not found"});
+      }
+      if(roleId)
+      {
+        const role = await Role.findById(roleId)
+        if(!role)
+          {
+            return res.status(404).json({message:"role not found"});
+        }
+      }
+      if(password)
+      {
+        const saltRounds = 10;
+        user.password = await bcrypt.hash(password,saltRounds)
+        
+      }
+
+      user.username = username || user.username;
+      user.email = email || user.email;
+      user.phone = phone || user.phone;
+      user.roleId = roleId || user.roleId;
+
+      const updatedUser = await user.save();
+
+    // Update user profile if provided
+    const userProfile = await UserProfile.findOne({ userId: updatedUser._id });
+    if (userProfile) {
+      userProfile.firstName = firstName || userProfile.firstName;
+      userProfile.lastName = lastName || userProfile.lastName;
+      userProfile.dob = dob || userProfile.dob;
+      userProfile.contactNumber = contactNumber || userProfile.contactNumber;
+      await userProfile.save();
+    }
+
+    // Respond with success
+    return res.status(200).json({
+      message: "Employee updated successfully",
+      user: updatedUser,
+      profile: userProfile,
+    });
+      
+    } catch (error) {
+
+      return res
+      .status(500)
+      .json({ message: "Error updating employee", error: error.message });
+      
+    }
+    
+  }
 }
 
 module.exports = employeeController;
--- node-app/routes/employeeRoutes.js
+++ node-app/routes/employeeRoutes.js
@@ -9,6 +9,7 @@ router.post('/employees/add',authMiddleware("create"), employeeController.addEmp
 router.get('/employees/view/:id',authMiddleware("view"), employeeController.viewEmployee);
  
 router.get('/employees/viewall/',authMiddleware("viewall"), employeeController.viewAllEmployees);
-router.delete('/employees/delte/:id',authMiddleware("delete"), employeeController.viewAllEmployees);
+router.delete('/employees/delte/:id',authMiddleware("delete"), employeeController.deleteEmployee);
+router.put('/employees/edit/:id',authMiddleware("update"), employeeController.updateEmployee);
 
 module.exports = router;
