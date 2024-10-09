ab50486ad4ab5c88bdd58a67f7a2fb0a3760b704
mdtanvir200 <mdtanvir.ahmed@teamgrid.io>
2024-10-09 14:43:10 +0530

    routes added for viewing organization

--- node-app/routes/authRoutes.js
+++ node-app/routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { signUp, addOrganization }=require('../controllers/authController');
+const { signUp, addOrganization, viewOrganization }=require('../controllers/authController');
 // const authMiddleware = require('../middleware/authJwt');
 const router = express.Router();
 
 router.post('/add',signUp);
 router.post('/addOrganization',addOrganization);
+router.get('/addOrganization/',viewOrganization);
 // router.post('/addOrganization',authMiddleware("create"),addOrganization);
 
 module.exports=router;
